95e464bf077cbd6c47fea29c950034fc0f1cd27b
Andrii Chernenko <andrii.chern@gmail.com>
2020-05-13 17:37:24 +0300

    Initial commit & base setup

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+// import App from "./components/App.jsx";
+// import url from "./styles/style.css";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "react-toastify/dist/ReactToastify.css";
+
+// render(<App />, document.getElementById("app"));
+
+module.hot.accept();
---
+++ webpack.config.babel.js
@@ -0,0 +1,62 @@
+import path from "path";
+import WebpackCleanupPlugin from "webpack-cleanup-plugin";
+import { HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  target: "web",
+  entry: path.join(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      //   {
+      //     test: /\.css$/,
+      //     use: [
+      //       {
+      //         loader: "style-loader/url",
+      //         options: { sourceMap: true },
+      //       },
+      //       {
+      //         loader: "file-loader",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     test: /\.(ico)$/,
+      //     use: "file-loader?name=assets/[name].[ext]",
+      //   },
+    ],
+  },
+  plugins: [
+    new WebpackCleanupPlugin(),
+    new HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    hot: true,
+    https: false,
+  },
+};
